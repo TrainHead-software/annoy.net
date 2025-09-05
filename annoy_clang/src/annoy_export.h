f63e596a3b66ada4511a385ca767b3524efc31d8
Artem Chevychelov <artyomche9@gmail.com>
2025-09-06 01:08:32 +0300

    Improve Linux build support

--- annoy_clang/src/annoy_export.h
+++ annoy_clang/src/annoy_export.h
@@ -1,11 +1,15 @@
-﻿#pragma once
+#pragma once
 
-#ifdef _WIN32
+#if defined(_WIN32) || defined(__CYGWIN__)
   #ifdef BUILDING_ANNOY_C_DLL
     #define ANNOY_API __declspec(dllexport)
   #else
     #define ANNOY_API __declspec(dllimport)
   #endif
+#elif defined(__GNUC__) || defined(__clang__)
+  // Ensure functions are visible when building shared library on Unix-like systems
+  #define ANNOY_API __attribute__((visibility("default")))
 #else
   #define ANNOY_API
 #endif
+
